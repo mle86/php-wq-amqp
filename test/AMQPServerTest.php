5c395c9ab027a0e187b3888499c8f0b46107a947
mle <maximilian@eul.cc>
2020-02-02 14:32:51 +0100

    test: use Travis CI

--- test/AMQPServerTest.php
+++ test/AMQPServerTest.php
@@ -13,7 +13,7 @@ class AMQPServerTest extends AbstractWorkServerAdapterTest
 
     public function checkEnvironment(): void
     {
-        $this->checkInDocker();
+        $this->checkInDockerOrTravis();
 
         $this->assertEquals(1, getenv('IS_MLE86_WQ_AMQP_TEST'),
             "Container env var IS_MLE86_WQ_AMQP_TEST not set -- is this the correct test container?");
